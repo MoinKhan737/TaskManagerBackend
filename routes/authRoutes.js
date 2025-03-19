fe37ca4c1f3157617eff1bf718553f0965ad209c
Moin Khan <moinkhan29727@gmail.com>
2025-03-19 16:14:16 +0530

    Updated backend code

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const { signup, login } = require('../controllers/authController');
-const { forgotPassword, resetPassword } = require("../controllers/passwordController");
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
-router.post("/forgot-password", forgotPassword);
-router.post("/reset-password/:token", resetPassword);
 
 module.exports = router;
